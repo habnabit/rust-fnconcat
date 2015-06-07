ca689992e00a41e3ae2a149073d52a4631ae9599
Aaron Gallagher <_@habnab.it>
2015-06-07 14:48:31 -0700

    Work on TokenTrees instead of ast structs.
    This makes things more forward-compatible because it doesn't rely on ast
    struct layout.

--- src/lib.rs
+++ src/lib.rs
@@ -3,19 +3,23 @@
 extern crate rustc;
 extern crate syntax;
 
-use syntax::codemap::{Spanned, Span};
+use std::rc::Rc;
+
+use syntax::codemap::Span;
 use syntax::parse::{PResult, token};
-use syntax::ast::{TokenTree, TtDelimited, TtToken};
-use syntax::{abi, ast};
+use syntax::ast::{Delimited, Ident, TokenTree, TtDelimited, TtToken};
 use syntax::ext::base::{ExtCtxt, MacResult, DummyResult, MacEager};
-use syntax::ptr::P;
-use syntax::owned_slice::OwnedSlice;
+use syntax::ext::quote::rt::ToTokens;
 use syntax::util::small_vector::SmallVector;
 use rustc::plugin::Registry;
 
 
 fn tt_vec(tts: &[TokenTree]) -> Vec<TokenTree> {
     tts.iter().map(|tt| tt.clone()).collect()
+}
+
+fn ident_of_ctx_and_span(cx: &mut ExtCtxt, span: Span, ident: &str) -> TokenTree {
+    TtToken(span, token::Ident(cx.ident_of(ident), token::Plain))
 }
 
 fn build_string_from_idents(target: &mut String, tts: &[TokenTree]) -> Result<(), Span> {
@@ -61,12 +65,12 @@ fn fnconcat(cx: &mut ExtCtxt, sp: Span, args: &[TokenTree])
             cx.span_err(delim_span, "empty identifier");
             return DummyResult::any(sp);
         }
-        TtToken(sp, token::Ident(cx.ident_of(concatenated.as_str()), token::Plain))
+        ident_of_ctx_and_span(cx, delim_span, concatenated.as_str())
     } else {
         cx.span_err(sp, "use square brackets (i.e. [..]) in place of the function name");
         return DummyResult::any(sp);
     };
-    fn_tokens.insert(0, TtToken(sp, token::Ident(cx.ident_of("fn"), token::Plain)));
+    fn_tokens.insert(0, ident_of_ctx_and_span(cx, sp, "fn"));
     let mut p = cx.new_parser_from_tts(&fn_tokens[..]);
     if let Some(i) = p.parse_item() {
         return MacEager::items(SmallVector::one(i));
@@ -91,14 +95,17 @@ fn pull_tts_from_paren_groups(tts: &[TokenTree]) -> PResult<Vec<Vec<TokenTree>>>
     Ok(ret)
 }
 
-fn parse_pats_and_types(cx: &mut ExtCtxt, tts: Vec<TokenTree>) -> PResult<Vec<(P<ast::Pat>, P<ast::Ty>)>> {
+fn parse_pats_and_types(cx: &mut ExtCtxt, span: Span, tts: Vec<TokenTree>) -> PResult<Vec<Vec<TokenTree>>> {
     let mut ret = Vec::new();
     let mut parser = cx.new_parser_from_tts(&tts[..]);
     loop {
         let pat = try!(parser.parse_pat_nopanic());
         try!(parser.expect(&token::Colon));
         let ty = try!(parser.parse_ty_nopanic());
-        ret.push((pat, ty));
+        let mut pat = pat.to_tokens(cx);
+        pat.push(TtToken(span, token::Colon));
+        pat.extend(ty.to_tokens(cx).into_iter());
+        ret.push(pat);
         if !try!(parser.eat(&token::Comma)) {
             break;
         }
@@ -106,11 +113,11 @@ fn parse_pats_and_types(cx: &mut ExtCtxt, tts: Vec<TokenTree>) -> PResult<Vec<(P
     Ok(ret)
 }
 
-fn parse_exprs(cx: &mut ExtCtxt, tts: Vec<TokenTree>) -> PResult<Vec<P<ast::Expr>>> {
+fn parse_exprs(cx: &mut ExtCtxt, tts: Vec<TokenTree>) -> PResult<Vec<Vec<TokenTree>>> {
     let mut ret = Vec::new();
     let mut parser = cx.new_parser_from_tts(&tts[..]);
     loop {
-        ret.push(try!(parser.parse_expr_nopanic()));
+        ret.push(try!(parser.parse_expr_nopanic()).to_tokens(cx));
         if !try!(parser.eat(&token::Comma)) {
             break;
         }
@@ -118,90 +125,63 @@ fn parse_exprs(cx: &mut ExtCtxt, tts: Vec<TokenTree>) -> PResult<Vec<P<ast::Expr
     Ok(ret)
 }
 
-fn let_of_pat_type_and_expr(pat: P<ast::Pat>, ty: P<ast::Ty>, expr: P<ast::Expr>, span: Span) -> P<ast::Stmt> {
-    let id = pat.id;
-    let local = ast::Local {
-        pat: pat,
-        ty: Some(ty),
-        init: Some(expr),
-        id: id,
-        span: span.clone(),
-        source: ast::LocalLet,
-    };
-    let decl = ast::DeclLocal(P(local));
-    let stmt = ast::StmtDecl(P(Spanned { node: decl, span: span.clone() }), id);
-    P(Spanned { node: stmt, span: span })
+fn let_of_pat_and_expr(cx: &mut ExtCtxt, span: Span, pat: Vec<TokenTree>, expr: Vec<TokenTree>) -> Vec<TokenTree> {
+    let mut ret = vec![ident_of_ctx_and_span(cx, span, "let")];
+    ret.extend(pat.into_iter());
+    ret.push(TtToken(span, token::Eq));
+    ret.extend(expr.into_iter());
+    ret.push(TtToken(span, token::Semi));
+    ret
 }
 
-fn unit_output(id: ast::NodeId, sp: Span) -> ast::FunctionRetTy {
-    let ty = ast::Ty {
-        id: id,
-        node: ast::TyTup(Vec::new()),
-        span: sp.clone(),
-    };
-    ast::Return(P(ty))
+fn test_fn_of_ident_and_block(cx: &mut ExtCtxt, span: Span, ident: String, block: Vec<TokenTree>) -> Vec<TokenTree> {
+    vec![
+        TtToken(span, token::Pound),
+        TtDelimited(span, Rc::new(Delimited {
+            delim: token::Bracket,
+            open_span: span,
+            tts: vec![ident_of_ctx_and_span(cx, span, "test")],
+            close_span: span,
+        })),
+        ident_of_ctx_and_span(cx, span, "fn"),
+        ident_of_ctx_and_span(cx, span, ident.as_str()),
+        TtDelimited(span, Rc::new(Delimited {
+            delim: token::Paren,
+            open_span: span,
+            tts: Vec::new(),
+            close_span: span,
+        })),
+        TtDelimited(span, Rc::new(Delimited {
+            delim: token::Brace,
+            open_span: span,
+            tts: block,
+            close_span: span,
+        })),
+    ]
 }
 
-fn test_fn_item_of_ident_and_block(sp: Span, ident: ast::Ident, block: P<ast::Block>) -> P<ast::Item> {
-    let id = block.id;
-    let fn_decl = P(ast::FnDecl {
-        inputs: Vec::new(),
-        output: unit_output(id, sp),
-        variadic: false,
-    });
-    let generics = ast::Generics {
-        lifetimes: Vec::new(),
-        ty_params: OwnedSlice::empty(),
-        where_clause: ast::WhereClause {
-            id: id,
-            predicates: Vec::new(),
-        },
-    };
-    let item_fn = ast::ItemFn(
-        fn_decl,
-        ast::Unsafety::Normal,
-        ast::Constness::NotConst,
-        abi::Rust,
-        generics,
-        block,
-        );
-    let test_attribute = ast::Attribute_ {
-        id: ast::AttrId(0),
-        style: ast::AttrOuter,
-        value: P(Spanned { node: ast::MetaWord(token::InternedString::new("test")), span: sp.clone() }),
-        is_sugared_doc: false,
-    };
-    P(ast::Item {
-        ident: ident,
-        attrs: vec![Spanned { node: test_attribute, span: sp.clone() }],
-        id: id,
-        node: item_fn,
-        vis: ast::Public,
-        span: sp,
-    })
-}
-
-fn do_parametrization(cx: &mut ExtCtxt, sp: Span, base_name: ast::Ident,
-                      params: &ast::Delimited, whole_block: TokenTree)
+fn do_parametrization(cx: &mut ExtCtxt, span: Span, base_name: Ident, params: &Delimited, block: Vec<TokenTree>)
                       -> PResult<Box<MacResult + 'static>> {
-    let mut items = Vec::new();
+    let mut tokens = Vec::new();
     let mut groups = try!(pull_tts_from_paren_groups(&params.tts[..]));
-    let param_types = try!(parse_pats_and_types(cx, groups.remove(0)));
-    let param_exprs_vec: Vec<Vec<P<ast::Expr>>> = try!(groups.into_iter().map(|tts| parse_exprs(cx, tts)).collect());
-    let block = try!(cx.new_parser_from_tts(&vec![whole_block][..]).parse_block());
+    let param_types = try!(parse_pats_and_types(cx, span, groups.remove(0)));
+    let param_exprs_vec: Vec<Vec<Vec<TokenTree>>> = try!(groups.into_iter().map(|tts| parse_exprs(cx, tts)).collect());
     for (e, exprs) in param_exprs_vec.into_iter().enumerate() {
-        let mut new_stmts = Vec::new();
-        for ((pat, ty), expr) in param_types.iter().cloned().zip(exprs.into_iter()) {
-            new_stmts.push(let_of_pat_type_and_expr(pat, ty, expr, sp))
+        let mut fn_block = Vec::new();
+        for (pat, expr) in param_types.iter().cloned().zip(exprs.into_iter()) {
+            fn_block.extend(let_of_pat_and_expr(cx, span, pat, expr).into_iter());
         }
-        new_stmts.extend(block.stmts.iter().cloned());
-        let param_block = P(ast::Block {
-            stmts: new_stmts,
-            .. (*block).clone()
-        });
+        fn_block.extend(block.iter().cloned());
         let fn_name = format!("{}_{}", base_name.as_str(), e);
-        let ident = cx.ident_of(fn_name.as_str());
-        items.push(test_fn_item_of_ident_and_block(sp.clone(), ident, param_block));
+        tokens.extend(test_fn_of_ident_and_block(cx, span, fn_name, fn_block).into_iter());
+    }
+    let mut parser = cx.new_parser_from_tts(&tokens[..]);
+    let mut items = Vec::new();
+    loop {
+        match parser.parse_item() {
+            Some(i) => items.push(i),
+            None => break,
+        }
     }
     Ok(MacEager::items(SmallVector::many(items)))
 }
@@ -226,7 +206,7 @@ fn parametrize_test(cx: &mut ExtCtxt, sp: Span, args: &[TokenTree])
                 cx.span_err(params_span, "need at least param types and one param");
                 return DummyResult::any(params_span);
             }
-            match do_parametrization(cx, sp, name.clone(), params, TtDelimited(block_span, block.clone())) {
+            match do_parametrization(cx, sp, name.clone(), params, block.tts.clone()) {
                 Ok(r) => return r,
                 Err(_) => {
                     cx.span_err(sp, "error while parsing");
